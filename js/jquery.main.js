70b3ad8b3da9fbf66d25331d324e4580c1da66bb
Sergey <kuzmenkos1986@gmail.com>
2016-05-24 12:14:39 +0300

    start01

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -3,6 +3,40 @@ $(function(){
     $('.swiper-container').each(function () {
         Slider($(this));
     });
+
+    $.each($('.map'), function () {
+
+        var myMap;
+
+        function init () {
+            myMap = new ymaps.Map('map', {
+                center: $('.map__item').eq(0).attr('data-coord').split(', '),
+                zoom: 12
+            });
+            myMap.controls
+                .add('zoomControl', { left: 5, bottom: 5 })
+                .add('typeSelector')
+                .add('searchControl', { top: 5, right: 100 })
+                .add('mapTools', { left: 35, bottom: 5 });
+
+            $.each($('.map__item'), function(i){
+                var curElem = $(this);
+
+                if (curElem.attr('data-coord')) {
+                    var coord = curElem.attr('data-coord').split(', ');
+
+                    myMap.geoObjects.add(new ymaps.Placemark(
+                        [coord[0], coord[1]],
+                        {   hintContent: "CКС Ломбард",
+                            balloonContentBody: curElem.find('a').text() }, {
+                        }
+                    ));
+                }
+            });
+        }
+        ymaps.ready(init);
+
+    });
 } );
 
 var Slider = function (obj) {
